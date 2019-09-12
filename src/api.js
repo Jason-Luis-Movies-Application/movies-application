15d340dac546e50c3853fa7f866d6ff77b27f23a
Jason Kempel <kempel.jason13@gmail.com>
2019-09-12 16:19:35 -0500

    delete functionality

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
-      .then(response => response.json());
+        .then(response => response.json());
+  },
+  deleteMovie: (id) => {
+    const movie = {id: id};
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movie),
+    };
+    return fetch(`/api/movies/${id}`, options)
   }
 };
-
--- src/index.js
+++ src/index.js
@@ -1,54 +1,51 @@
-/**
- * require style imports
- */
-
-const {getMovies} = require('./api.js');
-getMovies().then((movies) => {
-  $('#title').html('MOVIES');
-  movies.forEach(({title, rating, id}) => {
-    $('#loading').remove();
-    $('body').css('background-color','white');
-    $('.container').css('visibility','visible');
-    $('#movieDisplay').append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button class="button" id="edit${id}">Edit</button> / <button class="button" id="delete${id}">Delete</button></td></tr>`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
-
-function updateMovies() {
-  const {getMovies} = require('./api.js');
+const {getMovies,deleteMovie} = require('./api.js');
+function buildHTML () {
   getMovies().then((movies) => {
     $('#title').html('MOVIES');
+    $("#movieDisplay").html("");
     movies.forEach(({title, rating, id}) => {
       $('#loading').remove();
       $('body').css('background-color', 'white');
       $('.container').css('visibility', 'visible');
-      $('#movieDisplay').append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button class="button" id="${id}edit">Edit</button> / <button class="button" id="delete${id}">Delete</button></td></tr>`);
+      $('#movieDisplay').append(`<ul><li>ID: ${id}</li><li>Title: ${title}</li><li>Rating: ${rating}</li><button type="button" class="editButton">Edit</button> / <button type="button" class="deleteButton" id="${id}Delete">Delete</button></ul>`);
     });
+    editMovieInfo();
+    deleteMovieInfo();
   }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
   });
 }
 
+function updateMovies() {
+  buildHTML();
+}
 // ADD MOVIE
-let submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', addMovie);
-function addMovie(e) {
-  e.preventDefault();
-  let movie = document.getElementById('movieTitle').value;
-  let rating = document.getElementById('movieRating').value;
-  let data ={title: movie, rating: rating};
-  fetch('/api/movies', {method: 'POST', headers:{'Content-Type': 'application/json'}, body: JSON.stringify(data)})
-setTimeout(updateMovies, 500);
-}
-// EDIT MOVIE
-function editMovie() {
+  let submitButton = document.querySelector('#submit');
+  submitButton.addEventListener('click', addMovie);
 
-}
+const editMovieInfo = function() {
+  $('.editButton').click(function () {
 
-// LISTENERS
-// function addListeners() {
-//
-// }
+  });
+};
+
+
+const deleteMovieInfo = function() {
+  $('.deleteButton').click(function() {
+    let idNum = parseInt($(this).attr("id"));
+    console.log($(this));
+    deleteMovie(idNum);
+  });
+  setTimeout(updateMovies, 500);
+};
+
+  function addMovie(e) {
+    e.preventDefault();
+    let movie = document.getElementById('movieTitle').value;
+    let rating = document.getElementById('movieRating').value;
+    let data = {title: movie, rating: rating};
+    fetch('/api/movies', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+    setTimeout(updateMovies, 500);
+}
+buildHTML();
