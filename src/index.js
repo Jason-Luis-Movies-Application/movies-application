14dadba5e221133819586b5e6aad1fe4854f8837
Luis Delgado <delgadoluis0510@gmail.com>
2019-09-12 09:55:31 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 /**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
-
-/**
  * require style imports
  */
 const {getMovies} = require('./api.js');
-
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+  $('#title').html('Here are all the movies:');
+  // movies.forEach(({title, rating, id}) => {
+  //   $('#movieDisplay').html(`id#${id} - ${title} - rating: ${rating}`);
+  // });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+function buildMovieHTML (getMovies) {
+  return `<ul><li>Title: ${title}</li><li>Rating: ${rating}</li><li>ID#${id}</li>`
+}
+
+const addHTMLToPage = getMovies => {
+  for (let i = 0; i < getMovies.length; i += 1) {
+      const movieItems = document.getElementsByClassName('movieDisplay')[i];
+      movieItems.innerHTML = buildMovieHTML(movies[i]);
+    }
+};
+
+
