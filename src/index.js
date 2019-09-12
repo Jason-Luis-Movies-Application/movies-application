37deb58acb0a7f18aa948a69de56259a3f0c873e
Jason Kempel <kempel.jason13@gmail.com>
2019-09-12 11:17:48 -0500

    breakthrough

--- src/index.js
+++ src/index.js
@@ -4,22 +4,41 @@
 const {getMovies} = require('./api.js');
 getMovies().then((movies) => {
   $('#title').html('Here are all the movies:');
-  // movies.forEach(({title, rating, id}) => {
-  //   $('#movieDisplay').html(`id#${id} - ${title} - rating: ${rating}`);
-  // });
+  movies.forEach(({title, rating, id}) => {
+    $('#loading').remove();
+    $('body').css('background-color','white');
+    $('#userInput').css('visibility','visible');
+    $('#movieDisplay').append(`<ul><li>Title: ${title}</li><li>Rating: ${rating}</li><li>ID#${id}</li>`);
+  });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
-function buildMovieHTML (getMovies) {
-  return `<ul><li>Title: ${title}</li><li>Rating: ${rating}</li><li>ID#${id}</li>`
+
+let submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', addMovie);
+
+function addMovie(e) {
+  e.preventDefault();
+  let movie = document.getElementById('movieTitle').value;
+  let rating = document.getElementById('movieRating').value;
+  let data = {
+    title: movie,
+    rating: rating
+  };
+  document.getElementById('movieTitle').value = '';
+  document.getElementById('movieRating').value = '';
+  fetch('/api/movies', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+      .then((data) => {
+        console.log('Request success: ', data);
+      })
+      .catch((error) => {
+        console.log('Request failure: ', error);
+      });
 }
-
-const addHTMLToPage = getMovies => {
-  for (let i = 0; i < getMovies.length; i += 1) {
-      const movieItems = document.getElementsByClassName('movieDisplay')[i];
-      movieItems.innerHTML = buildMovieHTML(movies[i]);
-    }
-};
-
-
